27444db4f98bb8f38811f4aa88ef3edfda2cc1b4
Felipe Valenzuela <feelipevalenzuela@hotmail.com>
2026-05-13 01:16:51 -0300

    Refactor authMiddleware, add authMiddleware.test

--- src/shared/middleware/authMiddleware.js
+++ src/shared/middleware/authMiddleware.js
@@ -1,17 +1,33 @@
 const jwt = require('jsonwebtoken');
+const logger = require('../utils/logger');
+
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ error: 'Token requerido' });
+        return res.status(401).json({error: 'No autorizado', message: 'Token requerido' });
     }
 
     const token = authHeader.split(' ')[1];
     try {
-        req.user = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = {
+            id: decoded.id,
+            email: decoded.email,
+            role: decoded.role
+        }
         next();
-    } catch {
-        return res.status(401).json({ error: 'Token inválido o expirado' });
+    } catch (error) {
+        logger.error('Token inválido o expirado', { error });
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ 
+                error: 'Token expirado', 
+                message: 'El token ha caducado, por favor solicita uno nuevo.'
+             });
+        }
+        return res.status(401).json({
+            error: 'Token inválido', 
+            message: 'La autenticación ha fallado.' });
     }
 };
 
