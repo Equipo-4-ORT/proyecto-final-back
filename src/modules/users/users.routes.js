bf8b9e23e31eae41745951737dcbcf44f1ecebb8
LicFuraca <martinexequield@gmail.com>
2026-05-16 14:50:36 -0300

    agrego rate limit en users routes

--- src/modules/users/users.routes.js
+++ src/modules/users/users.routes.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { authMiddleware } = require('../../shared/middleware');
+const { apiLimiter } = require('../../shared/middleware/rateLimiter');
 
 const router = express.Router();
 
+router.use(apiLimiter);
 router.use(authMiddleware);
 
 // Aquí irían las rutas de usuarios
