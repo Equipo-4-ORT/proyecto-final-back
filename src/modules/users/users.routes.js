106b2c1b688574b7332c95f86e0b9b91ba71d521
Felipe Valenzuela <feelipevalenzuela@hotmail.com>
2026-06-02 14:10:04 -0300

    fix router routes

--- src/modules/users/users.routes.js
+++ src/modules/users/users.routes.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const router = express.Router();
 const { authMiddleware } = require('../../shared/middleware');
 const { apiLimiter } = require('../../shared/middleware/rateLimiter');
 const { getSettings, updateSettings } = require('./users.controller');
